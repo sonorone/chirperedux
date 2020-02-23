084d0f34e16e3efc2b74535f165682d38ccc82d9
Damian Murawiecki <dmurawiecki@myseneca.ca>
2020-02-23 17:44:29 -0500

    implemented Like in Tweet

---
+++ src/reducers/tweets.js
@@ -0,0 +1,24 @@
+import { RECEIVED_TWEETS, TOGGLE_TWEET } from "../actions/tweets";
+
+export default function tweets(state = {}, action) {
+  switch (action.type) {
+    case RECEIVED_TWEETS:
+      return {
+        ...state,
+        ...action.tweets
+      };
+    case TOGGLE_TWEET:
+      return {
+        ...state,
+        [action.id]: {
+          ...state[action.id],
+          likes:
+            action.hasLiked === true
+              ? state[action.id].likes.filter(uid => uid !== action.authedUser)
+              : state[action.id].likes.concat([action.authedUser])
+        }
+      };
+    default:
+      return state;
+  }
+}
